d298816159516b36a0654ecf59b08aa49fefb3ae
Ryan <vellabonelloryan@gmail.com>
2022-07-09 11:14:30 +0800

    About page
    Adds a simple About Page

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="about">
+		<h2>About</h2>
+		<p>This project is my first attempt at tinkering around with Vue.</p>
+		<p>I haven't read any of the documentation yet. But I feel that I am understanding how this framework works.</p>
+		<p>Written by {{author}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'AboutPage',
+	data () {
+		return {
+			author: 'Ryan Vella Bonello'
+		}
+	}
+}
+</script>
