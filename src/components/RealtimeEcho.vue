aa14544ea71bf346b5753048f52e0e7dd1284714
Ryan <vellabonelloryan@gmail.com>
2022-07-09 16:50:21 +0800

    Realtime Echo page
    v-model

---
+++ src/components/RealtimeEcho.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		
+		<input type="text" name="" v-model="myInput" placeholder="write something...">
+		<h2>{{ myInput }}</h2>
+		<br>
+		<p>
+			<small>
+				<code>v-model</code> is a directive that creates a two-way data binding between a value in our template and a value in our data properties.
+				<a href="https://compiletab.com/vue-get-input-value/">Read more</a>
+			</small>
+		</p>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'RealtimeEcho',
+		data () {
+			return {
+				myInput: ""
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+input {
+	font-size: 25px;
+	padding: 20px;
+}
+
+h2 {
+	font-size: 50px;
+
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/components/HelloWorld'
 import AboutPage from '@/components/AboutPage'
 import FruitPage from '@/components/FruitPage'
 import CounterPage from '@/components/CounterPage'
+import RealtimeEcho from '@/components/RealtimeEcho'
 
 Vue.use(Router)
 
@@ -28,6 +29,11 @@ export default new Router({
       path: '/counter',
       name: 'CounterPage',
       component: CounterPage
+    },
+    {
+      path: '/realtime-echo',
+      name: 'RealtimeEcho',
+      component: RealtimeEcho
     }
   ]
 })
